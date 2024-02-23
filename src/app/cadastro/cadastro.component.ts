c0ac01164f91d97a771c6328a5fe955879e3e36b
mohamedorra <orrahamudi@gmail.com>
2024-02-22 23:20:03 -0300

    added code
    form pushed

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,48 @@
+import { ConsultaCepService } from './../service/consulta-cep.service';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  constructor(private router: Router, private consultaCepService : ConsultaCepService) { }
+
+  ngOnInit(): void {
+  }
+
+  consultaCEP(ev:any, f:NgForm){
+    const cep = ev.target.value;
+    if(cep !== ''){
+      this.consultaCepService.getConsultaCep(cep).subscribe(resultado => 
+        {
+          console.log(resultado);
+          this.populandoEndereco(resultado, f );
+        });
+    }
+    }
+
+    populandoEndereco(dados: any , form:NgForm){
+      form.form.patchValue({
+        endereco : dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado:dados.uf
+      });
+
+    }
+  
+  cadastrar(form : NgForm){
+     if(form.valid){
+    this.router.navigate(['./sucesso'])
+   }
+     else {
+     alert("Formulario Invalido");
+   }
+}     
+}
